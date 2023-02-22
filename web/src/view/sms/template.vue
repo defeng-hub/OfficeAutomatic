28ea1c89b00e6a7947aa3b1a2462a586535e9cf4
wdf <15555150781@163.com>
2023-02-22 14:05:49 +0800

    完善了sms 发送模板

--- web/src/view/sms/template.vue
+++ web/src/view/sms/template.vue
@@ -6,32 +6,35 @@
 			<div class="gva-btn-list">
 				<el-button type="primary" icon="refresh" @click="refreshData">刷新短信模板列表</el-button>
 			</div>
-			<el-table ref="multipleTable" stripe  highlight-current-row :data="tableData" style="width: 100%" tooltip-effect="dark" row-key="template_id">
+			<el-table ref="multipleTable" stripe highlight-current-row :data="tableData" style="width: 100%"
+				tooltip-effect="dark" row-key="template_id">
 				<el-table-column type="selection" width="50" />
-				<el-table-column align="center" label="模板ID"   prop="template_id" width="100" />
+				<el-table-column align="center" label="模板ID" prop="template_id" width="100" />
 				<el-table-column align="center" label="模板内容" prop="template_content" width="400" />
 				<el-table-column align="center" label="模板状态" prop="status_code" width="120" />
 				<el-table-column align="center" label="模板名称" prop="template_name" width="120" />
 				<el-table-column align="center" label="提交审核时间" prop="create_time" width="120" />
-				
+
 				<el-table-column align="center" label="按钮组" min-width="100">
 					<template #default="scope">
-            <el-button type="primary" link icon="edit" >发短信</el-button>
+						<el-button type="primary" link icon="edit" @click="openDialog(scope.row)">发短信</el-button>
 
 						<el-popover v-model="scope.row.visible" placement="top" width="160">
 							<p>确定要删除吗？</p>
 							<div style="text-align: right; margin-top: 8px;">
 								<el-button type="primary" link @click="scope.row.visible = false">取消</el-button>
-								<el-button type="primary" @click="deleteCustomer(scope.row)">确定</el-button>
+								<el-button type="primary" @click="deleteSms(scope.row)">确定</el-button>
 							</div>
 
+							<!-- 原始样式 -->
 							<template #reference>
 								<el-button type="primary" link icon="delete" @click="scope.row.visible = true">删除</el-button>
 							</template>
 						</el-popover>
+
 					</template>
 				</el-table-column>
-				
+
 			</el-table>
 			<div class="gva-pagination">
 				<el-pagination :current-page="page" :page-size="pageSize" :page-sizes="[10, 30, 50, 100]" :total="total"
@@ -39,6 +42,24 @@
 					@size-change="handleSizeChange" />
 			</div>
 		</div>
+
+		<el-dialog v-model="dialogFormVisible" :before-close="closeDialog" title="发送短信">
+
+			<el-tag type="default">{{'模板ID:'+ form.template_id }}</el-tag>
+
+			<el-row :gutter="20">
+				<el-col :span="8" v-for="obj,idx in form.tpl_params">
+					<el-input v-model="form.tpl_params[idx]" :placeholder="`参数${idx+1}`" ></el-input>
+				</el-col>
+			</el-row>
+
+			<template #footer>
+				<div class="dialog-footer">
+					<el-button @click="closeDialog">取 消</el-button>
+					<el-button type="primary" @click="enterDialog">确 定</el-button>
+				</div>
+			</template>
+		</el-dialog>
 
 
 
@@ -50,7 +71,7 @@ import {
 	deleteExaCustomer,
 } from '@/api/customer'
 
-import { UpdateTemplates,GetSmsList } from '@/api/txyun/sms'
+import { UpdateTemplates, GetSmsList } from '@/api/txyun/sms'
 import { ref } from 'vue'
 
 const page = ref(1)
@@ -83,29 +104,72 @@ const getTableData = async () => {
 getTableData()
 
 
-const type = ref('')
 
-
-const deleteCustomer = async (row) => {
-	row.visible = false
-	const res = await deleteExaCustomer({ ID: row.ID })
-	if (res.code === 0) {
-		ElMessage({
-			type: 'success',
-			message: '删除成功'
-		})
-		if (tableData.value.length === 1 && page.value > 1) {
-			page.value--
-		}
-		getTableData()
-	}
+const deleteSms = async (row) => {
+	console.log(row)
 }
+
+// dialog
+
+const form = ref({
+	template_id:"", //模板id
+	template_content:"",//模板内容
+	tpl_params:[],
+	parms_num:0,
+})
+const dialogFormVisible = ref(false)
+
+const closeDialog = async ()=>{
+	dialogFormVisible.value = false
+}
+const openDialog = (row) => {
+  dialogFormVisible.value = true;
+	form.value.parms_num = findParmsNum(row.template_content+"{2}+{3}");
+	form.value.template_content = row.template_content;
+	form.value.template_id = row.template_id;
+	form.value.tpl_params = []
+
+	for (let index = 0; index < form.value.parms_num; index++) {
+		form.value.tpl_params.push("")
+	}
+
+}
+const enterDialog = async() => {
+	console.log(form.value)
+
+}
+
+// 传入string,获取{1},{2}, 这个的最大数量
+const findParmsNum = (content) =>{
+	if(content.indexOf('{5}')!=-1){
+		if(content.indexOf('{6}')!=-1){
+			if(content.indexOf('{7}')!=-1){
+				if(content.indexOf('{8}')!=-1){
+					if(content.indexOf('{9}')!=-1){
+						return 9
+					}else{return 8}
+				}else{return 7}
+			}else{return 6}
+		}else{return 5}
+	}else{
+		if(content.indexOf('{4}')!=-1){return 4}else{
+			if(content.indexOf('{3}')!=-1){return 3}else{
+				if(content.indexOf('{2}')!=-1){return 2}else{
+					if(content.indexOf('{1}')!=-1){return 1}else{return 0}
+				}
+			}
+		}
+	}
+	
+}
+
 </script>
   
 
 <script>
 import WarningBar from '@/components/warningBar/warningBar.vue'
-import { UpdateTemplates,GetSmsList } from '@/api/txyun/sms'
+
+import { UpdateTemplates, GetSmsList } from '@/api/txyun/sms'
 import { ElMessage } from 'element-plus'
 export default {
 	name: 'sms',
@@ -134,7 +198,7 @@ export default {
 		}
 	},
 	created() {
-		console.log("aaa")
+		console.log("created")
 	}
 }
 
